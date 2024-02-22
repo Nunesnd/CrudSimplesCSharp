8a5ab80e27c759d9d062482193dc1f2539998e6e
Diego Nunes <nunesdiegond@gmail.com>
2024-02-22 09:11:17 -0300

    criando o modelo Pessoa e configurando os arquivos gitignore, CrusSimples.csproj e appsettings

---
+++ Models/PessoaModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class PessoaModel
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string Telefone { get; set; }
+    public string Email { get; set; }
+}
